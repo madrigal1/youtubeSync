df968bbd2a136965f4b58f3f795b0298724c7cfb
madrigal1 <kurupgokul11@gmail.com>
2021-07-10 22:10:26 +0530

    build:client

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,2 +1,3 @@
 /* export default 'https://agile-hamlet-78037.herokuapp.com'; */
-export default 'http://localhost:5000';
+// export default 'http://localhost:5000';
+export default 'https://youtube-sync-2.herokuapp.com';
